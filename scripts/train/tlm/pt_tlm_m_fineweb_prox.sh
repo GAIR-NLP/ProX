345eb3a9d2ace82d877d46919fa867cd91488fdc
koalazf99 <koala99.zf@gmail.com>
2024-09-25 06:09:48 +0000

    update prox train scripts

--- scripts/train/tlm/pt_tlm_m_fineweb_prox.sh
+++ scripts/train/tlm/pt_tlm_m_fineweb_prox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=pt_tlm_s_redpj_prox
+#SBATCH --job-name=pt_tlm_s_fineweb_prox
 #SBATCH --output=<expected_output_file>
 #SBATCH --partition=<your_partition>
 #SBATCH --error=<expected_error_file>
@@ -28,20 +28,20 @@ cd train
 export PYTHONPATH=$PYTHONPATH:$TINYLM_WORK_DIR/train
 
 srun python pretrain/tinyllama.py \
-    --config_path configs/general/pt_tlm_s_redpj_prox.yaml
+    --config_path configs/general/pt_tlm_m_fineweb_prox.yaml
 
 cd $TINYLM_WORK_DIR
 conda activate tinylm
 python -m scripts.weight_conversion.batch_model_conversion \
-    --litgpt_model_dir pt_llama_0_7b_redpj_25B_prox \
-    --hf_model_dir pt_llama_0_7b_redpj_25B_prox \
-    --save_token_interval 1 \
-    --arch_name tiny_LLaMA_0_7b
+    --litgpt_model_dir pt_llama_1_7b_fineweb_50B_prox \
+    --hf_model_dir pt_llama_1_7b_fineweb_50B_prox \
+    --save_token_interval 5 \
+    --arch_name tiny_LLaMA_1_7b
 
 # model step list is from 1 ~ 25
 export NNODES=8
-export TOTAL_STEPS=25
-export SAVE_STEP_INTERVAL=1
+export TOTAL_STEPS=50
+export SAVE_STEP_INTERVAL=5
 
 cd $TINYLM_WORK_DIR
 source $TINYLM_CONDA_DIR
@@ -80,7 +80,7 @@ done
 echo "Steps for node $node_id: $node_steps"
 export HUGGINGFACE_HUB_CACHE=$DATA_ROOT_DIR/huggingface/hub
 python -m scripts.eval.base_evaluation \
-    --hf_model_dir pt_llama_0_7b_redpj_25B_prox \
+    --hf_model_dir pt_llama_1_7b_fineweb_50B_prox \
     --task_impl lighteval \
     --task_set fineweb \
     --model_step_list $node_steps
